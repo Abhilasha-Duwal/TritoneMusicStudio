bb331f318d9675976408f4692263c9d51cd66cb1
Abhilasha-Duwal <abhilashaduwal@gmail.com>
2022-04-21 12:43:24 +0545

    chages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { ThemeProvider } from './context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
